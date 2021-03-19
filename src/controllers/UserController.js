a980a10e875c22d936af3f11cf6e322cda19ec13
yesklin <cd.santos.360@gmail.com>
2021-03-19 17:31:56 -0300

    feat(#88): added email and phone validation, added password hashing
    Co-authored-by: Gabriel <gabriel.luiz77888@gmail.com>

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,18 +1,35 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
 
   async create(request, response){
     const {email, password, phoneNumber} = request.body;
 
-    const newUser = new User({
-      email: email,
-      password: password,
-      phoneNumber: password
-    });
+    const emailCheck = await User.find({email:email});
+    const phoneNumberCheck = await User.find({phoneNumber:phoneNumber});
+    
+    if(emailCheck.length>0)response.send("Email already registered");
+    else if(phoneNumberCheck.length>0)response.send("Phone number already registered");
+    else if(password.length<6||password.length>12) response.send("Password doesn't meet requeriments");
+    
+    else{
 
-    const result = await newUser.save();
-    response.send(result);
+      const newUser = new User({ //creating new user
+        email: email,
+        password: password,
+        phoneNumber: phoneNumber
+      });
+
+      //hashing password
+      const salt = await bcrypt.genSalt(10);
+      newUser.password = await bcrypt.hash(newUser.password, salt);
+      
+      const savedUser = await newUser.save();
+      response.send(savedUser);
+      
+    }
+
   },
 
   async index(request, response){
