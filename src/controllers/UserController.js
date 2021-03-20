b0dee26f4d182bd1cea34502900c09e71603d3b6
yesklin <cd.santos.360@gmail.com>
2021-03-20 15:35:48 -0300

    feat(#88): added User controllers

---
+++ src/controllers/UserController.js
@@ -0,0 +1,63 @@
+const User = require('../models/User');
+
+
+ async function createUser(request,response){
+  const {email, password, phoneNumber} = request.body;
+    
+  try {
+   
+    const checkEmail = await User.findOne({email:email});
+    const checkPassword = await User.findOne({phoneNumber:phoneNumber});
+
+    if(await User.findOne({email:email})!=null)
+      response.status(400).send({error:'Email already registered'});
+
+    else if(await User.findOne({password:password})!=null)
+      response.status(400).send({error:'Cellphone number already registered'});
+    
+    else{
+      const newUser = new User(request.body);
+      const savedUser = await newUser.save();
+      savedUser.password = undefined;
+      response.send({savedUser});
+    }
+      
+
+  } catch (error) {
+      response.status(400).send({error: 'Registration failed'});
+  }
+
+}
+
+
+async function getUserById(request, response){
+
+  try {
+    const user = await User.findById(request.params.userId);
+    
+    if(user!=null) response.send({ user });
+    response.status(400).send({error: 'User not found'});
+    
+  } catch (error) {
+      response.status(400).send({error: 'Failed to look for user'});
+  }
+  
+}
+
+
+async function listUsers(request, response){
+
+  try{
+    const users = await User.find({});
+
+    users.forEach(user => {
+      user.password = undefined;
+    });
+    response.send({users});
+
+  }catch(error){
+    response.status(400).send({error: 'Failed to list users'});
+  }
+}
+
+module.exports = {createUser, getUserById, listUsers};
