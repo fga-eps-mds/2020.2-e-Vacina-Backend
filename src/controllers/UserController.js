1e5f76d5ffe25204bcf2706b221cd483c5ba580e
yesklin <cd.santos.360@gmail.com>
2021-03-22 12:52:46 -0300

    refact(#88): got rid of validations due to changes in User schema

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,31 +2,18 @@ const User = require('../models/User');
 
 
  async function createUser(request,response){
-  const {email, password, phoneNumber} = request.body;
     
   try {
-   
-    const checkEmail = await User.findOne({email:email});
-    const checkPassword = await User.findOne({phoneNumber:phoneNumber});
 
-    if(await User.findOne({email:email})!=null)
-      response.status(400).send({error:'Email already registered'});
-
-    else if(await User.findOne({password:password})!=null)
-      response.status(400).send({error:'Cellphone number already registered'});
-    
-    else{
       const newUser = new User(request.body);
       const savedUser = await newUser.save();
       savedUser.password = undefined;
       response.send({savedUser});
-    }
-      
+          
 
   } catch (error) {
-      response.status(400).send({error: 'Registration failed'});
+      response.status(400).send({error: error.message});
   }
-
 }
 
 
@@ -35,13 +22,16 @@ async function getUserById(request, response){
   try {
     const user = await User.findById(request.params.userId);
     
-    if(user!=null) response.send({ user });
-    response.status(400).send({error: 'User not found'});
+    if(user!=null) 
+      response.send({ user });
+
+    else 
+      response.status(400).send({error: 'User not found'});
+
     
   } catch (error) {
       response.status(400).send({error: 'Failed to look for user'});
-  }
-  
+  } 
 }
 
 
@@ -49,15 +39,35 @@ async function listUsers(request, response){
 
   try{
     const users = await User.find({});
-
-    users.forEach(user => {
-      user.password = undefined;
-    });
     response.send({users});
+
 
   }catch(error){
     response.status(400).send({error: 'Failed to list users'});
   }
 }
 
-module.exports = {createUser, getUserById, listUsers};
+
+async function updateUser(request, response){
+  try{
+ 
+    //updating user
+    const id = request.params.userId;
+    const upadate = request.body;
+    const options = {new: true};
+
+    const updatedUser = await User.findByIdAndUpdate(id, upadate, options);
+
+    if(updatedUser!=null)
+      response.send({updatedUser});
+    
+    else
+      response.status(400).send({error:'User not found'});
+  
+
+  }catch(error){ //if client sends an email/phoneNumber already registered, or Id sent is not in expected format
+    response.status(400).send({error: error.message});
+  }
+}
+
+module.exports = {createUser, getUserById, listUsers, updateUser};
