621eec5cca3129523f6a465accf11a0db635677d
yesklin <cd.santos.360@gmail.com>
2021-03-22 12:15:35 -0300

    refact(#88): added unique and select configs to User schema

--- src/models/User.js
+++ src/models/User.js
@@ -4,17 +4,23 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   email:  {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    lowercase: true
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    select: false
   },
   phoneNumber: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   }
 });
+
+
 
 UserSchema.pre('save', async function(next){
   const hash = await bcrypt.hash(this.password, 10);
