0e3858979c4aee5c141de2256d4c9f9785da4758
yesklin <cd.santos.360@gmail.com>
2021-03-29 15:16:12 -0300

    feat(#102): created auth routes

--- src/routes/BaseRoute.js
+++ src/routes/BaseRoute.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 
 // importar rotas
 const userRoutes = require('./UserRoute');
+const authRoutes = require('./AuthRoute');
 //
 
 const routes = Router();
 
 routes.use('/user', userRoutes);
+routes.use('/auth', authRoutes);
 
 
 
