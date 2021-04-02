0433e7b73a8920a8f484284016c4ea79f6c1af76
yesklin <cd.santos.360@gmail.com>
2021-04-02 18:23:04 -0300

    feat(#112): added Vacine Model
    Co-authored-by: Paulo <pv.silva23@hotmail.com>

---
+++ src/models/Vacine.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const VacineSchema = new mongoose.Schema({
+  
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  
+  description: {
+    type: String,
+    required: true,
+  },
+
+  numberOfDoses:{
+    type: Number,
+    required: true
+  },
+
+  preventDeseases:{
+    type: [String],
+    required: true,
+  },
+
+  recommendations:{
+    type: [String],
+    required: true,
+  }
+  
+});
+
+
+const Vacine = mongoose.model("Vacine", VacineSchema);
+
+module.exports = Vacine;
