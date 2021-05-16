a1b8c14620874d016e994a7a811f6315676eef00
ciro-c <ciro.fga@gmail.com>
2021-05-16 00:06:03 -0300

    fix(*): fixed TakenVaccine delete by id operation

--- src/controllers/TakenVaccineController.js
+++ src/controllers/TakenVaccineController.js
@@ -1,4 +1,3 @@
- const express = require('express');
  const TakenVaccine = require('../models/TakenVaccines');
 
  async function createTakenVaccine(request, response){
@@ -82,15 +81,13 @@
    }
  }
 
- async function deleteTakenVaccineByProfile(profileId){
+ async function deleteTakenVaccineByComponentId(queryVenue){
   try {
-    var MyObjectId = require('mongoose').Types.ObjectId;
-    var queryVenue = {profileId: new MyObjectId(profileId)};
     const takenVaccine = await TakenVaccine.find(queryVenue).deleteMany();
     return  takenVaccine;
   } catch (error) {
-     return "deu erro";
+     return console.log("deu erro");
   } 
 }
 
- module.exports = {deleteTakenVaccineByProfile,getTakenVaccineByProfile,createTakenVaccine, getTakenVaccineById, listTakenVaccines, updateTakenVaccine, deleteTakenVaccine};
+ module.exports = {deleteTakenVaccineByComponentId,getTakenVaccineByProfile,createTakenVaccine, getTakenVaccineById, listTakenVaccines, updateTakenVaccine, deleteTakenVaccine};
